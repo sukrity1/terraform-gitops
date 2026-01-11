962e4a529fd069ed4f8d7a4fc31abf4d4cab6425
sukrity1 <macold@Macs-MacBook-Pro.local>
2026-01-11 08:39:49 +0530

    network

--- terraform/resources/network.tf
+++ terraform/resources/network.tf
@@ -3,6 +3,6 @@ module "vpc" {
   cidr_block           = var.vpc_cidr
   az_count             = var.az_count
   availability_zones = slice(data.aws_availability_zones.available.names, 0, 2)
-  // availability_zones   = ["us-east-1a", "us-east-1b"] # Minimum 2 AZs 
   environment          = var.environment
+  // availability_zones   = ["us-east-1a", "us-east-1b"] # Minimum 2 AZs 
 }
